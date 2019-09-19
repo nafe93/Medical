88f8e84acecffa00f2464765d23fc62c89af04e6
nafe kzir <nafe93@windowslive.com>
2019-09-19 20:11:56 +0300

    add erythrocytes

--- I/main.py
+++ I/main.py
@@ -50,7 +50,14 @@ age_female = count_age(data_female['age (years)'], data_female['sex'])
 disease_age_male = count_age(data_male['age of disease debute (years)'], data_male['sex'])
 disease_age_female = count_age(data_female['age of disease debute (years)'], data_female['sex'])
 
-print(disease_age_female)
+# erythrocytes
+erythrocytes_male = count_age(data_male['erythrocytes'], data_male['sex'])
+erythrocytes_female = count_age(data_female['erythrocytes'], data_female['sex'])
+
+# test
+print(erythrocytes_male)
+print(data_xl['erythrocytes'].value_counts())
+
 # print age
 print(f"Age of female is {round(age_female, 3)} and age of male is {round(age_male, 3)}")
 
@@ -67,6 +74,10 @@ draw_hist(data_age_view, num_bins, color, text_x='Age', text_y='Number')
 data_disease_view = [data_male['age of disease debute (years)'], data_female['age of disease debute (years)']]
 draw_hist(data_disease_view, num_bins, color, text_x='Age', text_y='Number')
 
+# draw erythrocytes
+data_erythrocytes_view = [data_male['erythrocytes'], data_female['erythrocytes']]
+draw_hist(data_erythrocytes_view, num_bins, color, text_x='erythrocytes', text_y='Number')
+
 # draw bar of age
 values_age = [age_male, age_female]
 draw_bar(values_age, names, text_y='Average')
@@ -74,6 +85,10 @@ draw_bar(values_age, names, text_y='Average')
 # draw bar of disease
 values_disease = [disease_age_male, disease_age_female]
 draw_bar(values_disease, names, text_y='Average')
+
+# draw bar of erythrocytes
+values_erythrocytes = [erythrocytes_male, erythrocytes_female]
+draw_bar(values_erythrocytes, names, text_y='Average')
 
 
 
